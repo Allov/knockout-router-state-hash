d8fdbdad1c93d48debcc3c471812c251a60538ad
fcharest <frederik.charest@radio-canada.ca>
2016-05-27 14:58:25 -0400

    build tools

--- dist/router-state-hash.js
+++ dist/router-state-hash.js
@@ -1,48 +1,64 @@
-'use strict';
+(function (global, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(['exports', 'hasher'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(exports, require('hasher'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, global.hasher);
+        global.routerStateHash = mod.exports;
+    }
+})(this, function (exports, _hasher) {
+    'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    var _hasher2 = _interopRequireDefault(_hasher);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    var RouterStateHash = function RouterStateHash(router) {
+        var self = this;
+
+        self.router = router;
+    }; // Copyright (c) CBC/Radio-Canada. All rights reserved.
+    // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+    RouterStateHash.prototype.init = function () {
+        var self = this;
+
+        _hasher2.default.initialized.add(function (newHash, oldHash) {
+            self.router._navigate(newHash, oldHash);
+        });
+
+        _hasher2.default.changed.add(function (newHash, oldHash) {
+            self.router._navigate(newHash, oldHash);
+        });
+
+        _hasher2.default.init();
+    };
+
+    RouterStateHash.prototype.setUrlSilently = function (url) {
+        _hasher2.default.changed.active = false;
+        _hasher2.default.setHash(url);
+        _hasher2.default.changed.active = true;
+    };
+
+    RouterStateHash.prototype.setUrl = function (url) {
+        _hasher2.default.setHash(url);
+    };
+
+    RouterStateHash.prototype.setUrlWithoutGeneratingNewHistoryRecord = function (url) {
+        _hasher2.default.replaceHash(url);
+    };
+
+    exports.default = RouterStateHash;
 });
-
-var _hasher = require('hasher');
-
-var _hasher2 = _interopRequireDefault(_hasher);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var RouterStateHash = function RouterStateHash(router) {
-    var self = this;
-
-    self.router = router;
-}; // Copyright (c) CBC/Radio-Canada. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-RouterStateHash.prototype.init = function () {
-    var self = this;
-
-    _hasher2.default.initialized.add(function (newHash, oldHash) {
-        self.router._navigate(newHash, oldHash);
-    });
-
-    _hasher2.default.changed.add(function (newHash, oldHash) {
-        self.router._navigate(newHash, oldHash);
-    });
-
-    _hasher2.default.init();
-};
-
-RouterStateHash.prototype.setUrlSilently = function (url) {
-    _hasher2.default.changed.active = false;
-    _hasher2.default.setHash(url);
-    _hasher2.default.changed.active = true;
-};
-
-RouterStateHash.prototype.setUrl = function (url) {
-    _hasher2.default.setHash(url);
-};
-
-RouterStateHash.prototype.setUrlWithoutGeneratingNewHistoryRecord = function (url) {
-    _hasher2.default.replaceHash(url);
-};
-
-exports.default = RouterStateHash;
